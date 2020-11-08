1f6d4c8a5a643384162c80691bdec2eecde9b99a
DavidReps <70821947+DavidReps@users.noreply.github.com>
2020-11-07 20:47:19 -0500

    Update MNIST.py

--- MNIST.py
+++ MNIST.py
@@ -1,3 +1,20 @@
+# Your task is to write a Python program that loads the digits data set, initializes a
+# model that is an instance of the sklearn.linear_model.SGDClassifier class, and
+# then carries out 50 stochastic gradient descent training iterations, as follows. In each
+# iteration, a random subset of 100 data examples (rows of the data set) should be
+# selected via the random.choices function; the SGDClassifier.partial_fit method
+# should then be used to train the model, using only the mini-batch consisting of the
+# 100 data rows selected in that iteration, together with their target labels (once you’ve
+# obtained the row numbers to use from choices and placed them in indices, say, you
+# can just use the syntax A[indices,:] to extract the corresponding rows of A).
+# Use the SGDClassifier.score method to keep track of the classification accuracy of
+# the model relative to the entire data set at the end of each training iteration. Report the
+# 50 accuracy scores obtained during the whole training procedure (one per iteration).
+# Plot the accuracy as a function of the cumulative number of training iterations. You
+# may use matplotlib.pyplot.plot and matplotlib.pyplot.show for this purpose.
+# Study the online documentation. Submit your code and results.
+
+
 import random
 import numpy as np
 import matplotlib.pyplot as plt
